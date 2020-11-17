d0d8495b76c9c02df3a878eb9f3d0407fd0e00c7
JeongWon <hjww1121@gamil.com>
2020-11-17 09:36:41 +0900

    11-17 merge

--- src/main/java/com/example/demo/dao/SubscriptionDAO.java
+++ src/main/java/com/example/demo/dao/SubscriptionDAO.java
@@ -1,23 +1,10 @@
 package com.example.demo.dao;
 
-<<<<<<< HEAD
 import java.util.List;
 import java.util.Vector;
 
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.Subscription.Subscription;
-@Repository("com.example.demo.dao.SubscriptionDAO")
-public interface SubscriptionDAO {
-	
-	public List<Subscription> showSubscriptionCustomer() throws Exception;;
-=======
-import java.util.Vector;
-import java.util.List;
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.model.Insurance.Insurance.InsuranceType;
-import com.example.demo.model.Insurance.Insurance;
 import com.example.demo.model.Subscription.Subscription;
 
 
@@ -29,7 +16,7 @@ public interface SubscriptionDAO{
 	public Vector<String> InsuranceTypeVector(int customerID);
 
 	public void insertSubscription(int insuranceID, int customerID);
-	
->>>>>>> kim
+
+	public List<Subscription> showSubscriptionCustomer();
 
 }
--- src/main/java/com/example/demo/service/InsuranceTreatmentService.java
+++ src/main/java/com/example/demo/service/InsuranceTreatmentService.java
@@ -1,6 +1,5 @@
 package com.example.demo.service;
 import java.util.List;
-import java.util.Vector;
 
 import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
